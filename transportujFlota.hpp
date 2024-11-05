25d115fcd7747fec6a0147bfb8af4aa741668869
Kajetan Gorczyca <01171650@pw.edu.pl>
2024-11-05 01:12:15 +0100

    zejsciowka_lab_3

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,24 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0) return 0;
+
+    unsigned int sumarycznyTowar = 0;
+    unsigned int liczbaZaglowcow = 0; 
+    Stocznia stocznia;
+
+    while (sumarycznyTowar < towar) {
+    
+        Statek* statek = stocznia();
+
+        sumarycznyTowar += statek->transportuj();
+
+        if (dynamic_cast<Zaglowiec*>(statek) != nullptr) {
+            ++liczbaZaglowcow;
+        }
+
+        delete statek;
+    }
+
+    return liczbaZaglowcow;
 }
